7842878ebccbebbd7cb4289fc8b194c9e8c8909c
barbara <barbara.prusiewicz@gmailcom>
2019-03-15 13:58:14 -1000

    Set up mocha environment and placeholder test file

---
+++ scripts/tests/test.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+import Environment from '../model/environment.js';
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe('Environment', function() {
+    describe('constructor', function() {
+      it('should return an object', function() {
+        let env = new Environment();
+        assert.notEqual(undefined,env);
+      });
+    });
+  });
+  
