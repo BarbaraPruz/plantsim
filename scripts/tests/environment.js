21b2c4be95fa1980a8871210eafb871f5f537be4
barbara <barbara.prusiewicz@gmailcom>
2019-03-16 15:43:48 -1000

    Add environment tests for update method

--- scripts/tests/environment.js
+++ scripts/tests/environment.js
@@ -3,22 +3,54 @@ import Environment from '../model/environment.js';
 
 describe('Environment', function() {
     describe('constructor', function() {
+      let env = new Environment();
       it('should return an object', function() {
-        let env = new Environment();
         assert.notEqual(undefined,env);
       });
       it('should set moisture', function() {
-        let moisture = new Environment().getMoisture();
-        assert.equal(50,moisture);
+        assert.equal(50,env.getMoisture());
       });
       it('should set light', function() {
-        let light = new Environment().getLight();
-        assert.equal(50,light);
+        assert.equal(50,env.getLight());
       });
       it('should set pH', function() {
-        let pH = new Environment().getpH();
-        assert.equal(6,pH);
+        assert.equal(6,env.getpH());
       });
+    });
+    describe('updates values',function() {
+      let env = new Environment();
+      it('should reduce moisture daily', function() {
+        let m1=env.getMoisture();
+        env.update({});
+        assert.equal(m1-5,env.getMoisture());
+      });
+      it('should update moisture on value',function() {
+        let m1=env.getMoisture();
+        env.update({water: "high"});
+        assert.equal(m1+5,env.getMoisture()); // high=+10 but daily reduces -5
+      });
+      it('should update light on value', function() {
+        env.update({light: "75"});
+        assert.equal(75,env.getLight());
+      });
+      // pH daily adjust for "mid level" moisture is +.08
+      it('should update pH on peat', function(){
+        let p1=env.getpH();
+        env.update({fertilizer: "peat"})
+        assert.equal(p1-0.25+0.08,env.getpH());
+      });
+      it('should update pH on lime', function(){
+        let p1=env.getpH();
+        env.update({fertilizer: "lime"})
+        assert.equal(p1+0.25+0.08,env.getpH());
+      });      
+      // to add - tests for border conditions
+    });
+    describe('can be reset',function() {
+
+    });
+    describe('is observable', function() {
+
     });
   });
   
